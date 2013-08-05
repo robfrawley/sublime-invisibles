67e4ad134735d1d8db0e3b83db851c0cdcb3acee
Rob M Frawley 2nd <rfrawley@scribenet.com>
2013-08-05 12:44:27 -0400

    updates to menu item to support toggle behavior

--- sublime-invisibles.py
+++ sublime-invisibles.py
@@ -7,3 +7,8 @@ class ToggleInvisiblesCommand(sublime_plugin.ApplicationCommand):
         white_space = "selection" if settings.get("draw_white_space", "selection") != "selection" else "all"
         settings.set("draw_white_space", white_space)
         sublime.save_settings("Preferences.sublime-settings")
+        return "off" if white_space == "selection" else "on"
+
+    def is_checked(self):
+    	settings = sublime.load_settings("Preferences.sublime-settings")
+        return settings.get("draw_white_space", "selection") != "selection"
